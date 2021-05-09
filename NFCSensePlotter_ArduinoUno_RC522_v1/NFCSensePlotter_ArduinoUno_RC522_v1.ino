b40e0f812ad4f519ee8e339a6ac47ad41f664016
Rong-Hao Liang <hyaloid@gmail.com>
2021-05-09 02:21:40 +0200

    Update ArduinoCodes

--- NFCSensePlotter_ArduinoUno_RC522_v1/NFCSensePlotter_ArduinoUno_RC522_v1.ino
+++ NFCSensePlotter_ArduinoUno_RC522_v1/NFCSensePlotter_ArduinoUno_RC522_v1.ino
@@ -1,11 +1,17 @@
 //*********************************************
-// Arduino Codes for NFCSense, which is based on
+// Arduino Codes for NFCSense,  which is based on
 // - NXP MFRC522 NFC/RFID Reader (e.g., RFID RC522)
 // - RFID.cpp Library (created by Dr. Leong and Miguel Balboa)
 // for further information please check 
 // our website: https://ronghaoliang.page/NFCSense
 // or contact Dr. Rong-Hao Liang (TU Eindhoven) via r.liang@tue.nl
 //*********************************************
+//Copyright <2021> Dr. Rong-Hao Liang
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+//IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+//WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <SPI.h> 
 #include "RFID.h"
